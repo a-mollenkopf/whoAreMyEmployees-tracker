ddb2849fccf9da38b98560521957ab867f61e2d6
Alexis Mollenkopf <alexismollenkopf@gmail.com>
2020-07-09 15:20:48 -0400

    switched to develop branch and added server boilerplate

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const cTable = require("console.table");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "6646023Ava!",
+    database: "employeeDB"
+  });
